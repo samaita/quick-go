0b1ff1a4289aaccea6750a6bada949938e32b354
unknown <garysamaita@gmail.com>
2021-04-02 23:56:19 +0700

    Create example login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
@@ -19,7 +19,7 @@ function App() {
 		firebase.initializeApp(firebaseConfig);
 	})
 
-	const LoginWithGoggle = () => {
+	const handleLoginWithGoggle = () => {
 		var provider = new firebase.auth.GoogleAuthProvider();
 		provider.addScope("profile");
 		provider.addScope("email");
@@ -52,8 +52,39 @@ function App() {
 
 	return (
 		<div>
-			<h1>Hello World</h1>
-			<a href="#" onClick={() => LoginWithGoggle()}>LOGIN</a>
+			<LoginPage
+				handleLoginWithGoogle={handleLoginWithGoggle} />
+		</div>
+	)
+}
+
+const LoginPage = ({
+	handleLoginWithGoogle
+}) => {
+	return (
+		<div className="bg-gray-200">
+			<div className="flex h-screen justify-center items-center">
+				<div className="m-auto w-5/12 h-96 flex shadow-md rounded-md">
+					<div className="w-5/12 h-full bg-gray-100 rounded-l-md p-5">
+						<span className="w-24 h-24 block m-auto mb-3 text-green-500">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path d="M10 3.5a1.5 1.5 0 013 0V4a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-.5a1.5 1.5 0 000 3h.5a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-.5a1.5 1.5 0 00-3 0v.5a1 1 0 01-1 1H6a1 1 0 01-1-1v-3a1 1 0 00-1-1h-.5a1.5 1.5 0 010-3H4a1 1 0 001-1V6a1 1 0 011-1h3a1 1 0 001-1v-.5z" />
+							</svg>
+						</span>
+						<form>
+							<input className="w-full h-9 mb-2 p-2 shadow-inner rounded-md text-gray-600 font-light focus:border-transparent" type="email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" placeholder="your@email.com" />
+							<input className="w-full h-9 mb-2 p-2 shadow-inner rounded-md text-gray-600 font-light focus:border-green-500" type="password" required placeholder="password" />
+							<button className="bg-green-500 text-white text-md font-light w-full p-2 rounded-md mb-1" type="submit">Login</button>
+						</form>
+						<span className="text-gray-600 font-light text-xs text-center w-full block mb-2">----- or login with -----</span>
+						<button className="bg-red-500 text-white text-sm font-light w-full p-2 rounded-md mb-2" onClick={() => handleLoginWithGoogle()}>Google</button>
+						<button className="bg-blue-500 text-white text-sm font-light w-full p-2 rounded-md">Facebook</button>
+					</div>
+					<div className="w-7/12 h-full bg-green-500 rounded-r-md">
+
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
