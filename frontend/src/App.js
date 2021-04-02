ff0727c4a50569cc00c82347ba6fb71e7c447322
unknown <garysamaita@gmail.com>
2021-04-03 00:17:52 +0700

    Adding logout page, hide login when loggedIn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,10 @@ import './App.css'
 
 function App() {
 
+	const [loggedIn, setLoggedIn] = useLocalStorage("loggedIn", false),
+		[credential, setCredential] = useLocalStorage("credential", {}),
+		[googleUser, setGoogleUser] = useLocalStorage("googleUser", {})
+
 	useEffect(() => {
 		let firebaseConfig = {
 			apiKey: process.env.REACT_APP_QUICK_GO_FIREBASE_API_KEY,
@@ -16,7 +20,11 @@ function App() {
 			messagingSenderId: process.env.REACT_APP_QUICK_GO_FIREBASE_MESSAGING_SENDER_ID,
 			appId: process.env.REACT_APP_QUICK_GO_FIREBASE_APP_ID
 		};
-		firebase.initializeApp(firebaseConfig);
+		if (!firebase.apps.length) {
+			firebase.initializeApp(firebaseConfig);
+		} else {
+			firebase.app(); // if already initialized, use that one
+		}
 	})
 
 	const handleLoginWithGoggle = () => {
@@ -34,7 +42,9 @@ function App() {
 				// The signed-in user info.
 				var user = result.user;
 
-				console.log(user, token, credential)
+				setCredential(credential)
+				setGoogleUser(user)
+				setLoggedIn(true)
 				// ...
 			}).catch((error) => {
 				// Handle Errors here.
@@ -50,20 +60,87 @@ function App() {
 			});
 	}
 
+	const handleLogout = () => {
+		firebase.auth().signOut().then(() => {
+			setCredential({})
+			setGoogleUser({})
+			setLoggedIn(false)
+		}).catch((error) => {
+			// An error happened.
+		});
+
+	}
+
+	function useLocalStorage(key, initialValue) {
+		const [storedValue, setStoredValue] = useState(() => {
+			try {
+				const item = window.localStorage.getItem(key);
+				return item ? JSON.parse(item) : initialValue;
+			} catch (error) {
+				console.log(error);
+				return initialValue;
+			}
+		});
+
+		const setValue = value => {
+			try {
+				const valueToStore =
+					value instanceof Function ? value(storedValue) : value;
+				setStoredValue(valueToStore);
+				window.localStorage.setItem(key, JSON.stringify(valueToStore));
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		return [storedValue, setValue];
+	}
+
 	return (
 		<div>
-			<LoginPage
-				handleLoginWithGoogle={handleLoginWithGoggle} />
+			<div className="bg-gray-200">
+				<LoginPage
+					isLogin={loggedIn}
+					handleLoginWithGoogle={handleLoginWithGoggle} />
+				<Dashboard
+					isLogin={loggedIn}
+					user={googleUser}
+					handleLogout={handleLogout} />
+			</div>
+		</div>
+	)
+}
+
+const Dashboard = ({ isLogin, user, handleLogout }) => {
+	return (
+		<div>
+			{isLogin && <div className="flex h-screen justify-center items-center">
+				<div className="m-auto w-5/12 h-96 flex shadow-md rounded-md">
+					<div className="w-5/12 h-full bg-gray-100 rounded-l-md p-5">
+						<span className="w-24 h-24 block m-auto mb-3 text-green-500">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path d="M10 3.5a1.5 1.5 0 013 0V4a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-.5a1.5 1.5 0 000 3h.5a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-.5a1.5 1.5 0 00-3 0v.5a1 1 0 01-1 1H6a1 1 0 01-1-1v-3a1 1 0 00-1-1h-.5a1.5 1.5 0 010-3H4a1 1 0 001-1V6a1 1 0 011-1h3a1 1 0 001-1v-.5z" />
+							</svg>
+						</span>
+						<h6 className="mb-2">Welcome, {user.displayName}</h6>
+						<button className="bg-red-500 text-white text-sm font-light w-full p-2 rounded-md mb-2" onClick={() => handleLogout()}>Logout</button>
+					</div>
+					<div className="w-7/12 h-full bg-green-500 rounded-r-md">
+
+					</div>
+				</div>
+			</div>}
 		</div>
 	)
 }
 
 const LoginPage = ({
+	isLogin,
 	handleLoginWithGoogle
 }) => {
 	return (
-		<div className="bg-gray-200">
-			<div className="flex h-screen justify-center items-center">
+		<div>
+			{!isLogin && <div className="flex h-screen justify-center items-center">
 				<div className="m-auto w-5/12 h-96 flex shadow-md rounded-md">
 					<div className="w-5/12 h-full bg-gray-100 rounded-l-md p-5">
 						<span className="w-24 h-24 block m-auto mb-3 text-green-500">
@@ -84,7 +161,7 @@ const LoginPage = ({
 
 					</div>
 				</div>
-			</div>
+			</div>}
 		</div>
 	)
 }
